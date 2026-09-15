2432cc3a2efa75e0c7e81e0b004a0d0bda445096
tiagooliveirajs <tiagooliveirajs@yahoo.com>
2026-09-15 02:13:04 +0100

    G01: enable TinyUSB CDC diagnostics

--- src_fw/picow_ble_usb_hid_bridge/tusb_config.h
+++ src_fw/picow_ble_usb_hid_bridge/tusb_config.h
@@ -92,10 +92,14 @@
 
 //------------- CLASS -------------//
 #define CFG_TUD_HID               1
-#define CFG_TUD_CDC               0
+#define CFG_TUD_CDC               1
 #define CFG_TUD_MSC               0
 #define CFG_TUD_MIDI              0
 #define CFG_TUD_VENDOR            0
+
+// CDC buffers used only for the Gate 01 diagnostic console.
+#define CFG_TUD_CDC_RX_BUFSIZE    64
+#define CFG_TUD_CDC_TX_BUFSIZE    256
 
 // HID buffer size Should be sufficient to hold ID (if any) + Data
 // @@chg
